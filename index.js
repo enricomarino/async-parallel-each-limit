1b8cb6dec9df07fa813701f6d39e59bb86bb7210
Enrico Marino <enrico.marino@email.com>
2013-10-04 02:32:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
-
-/* !
- * parallel-each-till
- * async parallel each till limit
- *
- * @copyright 2013 Enrico Marino
- * @license MIT
- */
-
-/*
- * Expose `parallel_each_till`
- */
-
-module.exports = parallel_each_till;
-
-/*
- * parallel_each_till
+/**
  * Apply `iterator` to the first `limit` items in `array`
  * and call `callback` when done
  *
@@ -25,7 +9,7 @@ module.exports = parallel_each_till;
  * @api public
  */
 
-function parallel_each_till (array, limit, iterator, callback) {
+module.exports = function (array, limit, iterator, callback) {
   var completed = 0;
   var started = 0;
   var running = 0;
@@ -43,7 +27,7 @@ function parallel_each_till (array, limit, iterator, callback) {
       callback();
       return;
     }
-  };
+  }
 
   while (running < limit && started < len) {
     iterator(array[started], complete);
